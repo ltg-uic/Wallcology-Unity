4a926002eeb2967c099c36ce9edab45eb4c93357
Gianluca Venturini <gianluca.91@gmail.com>
2015-10-06 18:35:57 -0500

    Added js support

---
+++ Assets/Scripts/NutellaInterface.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace CompleteProject
+{
+	public class NutellaInterface : MonoBehaviour
+	{
+		public GameObject enemy;                // The enemy prefab to be spawned.
+		//		public int citterAmount = 3;			//The number of critters on the scene
+		public float spawnTime = 3f;            // How long between each spawn.
+		public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+	
+
+		void test(int param)
+		{
+			Debug.Log(param);
+			this.Spawn ();
+		}
+
+		
+		void Start ()
+		{
+			// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+			InvokeRepeating ("Spawn", spawnTime, spawnTime);
+			Debug.Log ("here");
+			
+		}
+		
+		
+		void Spawn ()
+		{
+			// If the player has no health left...
+			//            if(playerHealth.currentHealth <= 0f)
+			//            {
+			//                // ... exit the function.
+			//                return;
+			//            }
+			
+			
+			// Find a random index between zero and one less than the number of spawn points.
+			int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+			
+			// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+			Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+		}
+	}
+}
